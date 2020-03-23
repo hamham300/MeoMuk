4c55a8daac7a2bb6322ee3b537ec23e3dac0f7fb
Jeon <hamham300@naver.com>
2020-03-23 15:42:40 +0900

    Jeon 2020-03-23 15:42 click price button layout + clickevebt

--- app/src/main/java/kr/co/hwakjjin/myapplication/MainActivity.java
+++ app/src/main/java/kr/co/hwakjjin/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package kr.co.hwakjjin.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button btn_price = (Button) findViewById(R.id.btn_price);
+        final Button btn_menu = (Button) findViewById(R.id.btn_menu);
+        final Button btn_back = (Button) findViewById(R.id.btn_back);
+        final LinearLayout layout_priceSelect = (LinearLayout) findViewById(R.id.price_select_layout);
+
+
+        btn_price.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout_priceSelect.setVisibility(View.VISIBLE);
+                btn_back.setVisibility(View.VISIBLE);
+                btn_menu.setVisibility(View.INVISIBLE);
+                btn_price.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        btn_back.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                layout_priceSelect.setVisibility(View.INVISIBLE);
+                btn_back.setVisibility(View.INVISIBLE);
+                btn_menu.setVisibility(View.VISIBLE);
+                btn_price.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
